96f57cb32256d8ac9972f27cf2e45db70355bf7f
zairelyshdz <zairelizh@gmail.com>
2025-04-09 14:24:40 -0400

    Changes to be committed: 	new file:   .vscode/settings.json 	new file:   img/banfanb-logo.png 	new file:   img/banfanb-seeklogo.cdr 	new file:   img/menu-icono.png 	modified:   prueba2.html 	modified:   src/script.js 	modified:   styless/css.css

--- src/script.js
+++ src/script.js
@@ -22,3 +22,16 @@ document.addEventListener("click",(event)=> {
         mainContent.style.marginLeft= "0";
     }
 });
+
+var header = document.getElementById("Header");
+
+window.addEventListener('scroll', () => {
+    
+    var scroll = window.scrollY;
+    if (scroll > 10) {
+        header.style.backgroundColor = '#121212';
+    }    else {
+        header.style.backgroundColor = 'transparent';
+    }
+})
+
